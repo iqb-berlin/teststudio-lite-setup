d97cff31358845e83e06e8536b831f39748ca9d8
rhenck <richard.henck@iqb.hu-berlin.de>
2021-03-22 14:00:19 +0100

    Update version to {@0.0.2

--- dist/install.sh
+++ dist/install.sh
@@ -33,6 +33,39 @@ else
   echo "Make found"
 fi
 
+### Download package ###
+DOWNLOAD='y'
+if ls teststudio-lite-*.tar 1> /dev/null 2>&1
+  then
+    PACKAGE_FOUND=true
+    if [ $(ls teststudio-lite-*.tar | wc -l) -gt 1 ]
+      then
+        echo "Multiple packages found. Remove all but the one you want!"
+        exit 1
+    fi
+    read -p "Installation package found. Do you want to check for and download the latest release anyway? [Y/n]:" -e DOWNLOAD
+  else
+    PACKAGE_FOUND=false
+    read -p "No installation package found. Do you want to download the latest release? [Y/n]:" -e DOWNLOAD
+fi
+
+if [ "$PACKAGE_FOUND" = 'false' ] && [ "$DOWNLOAD" = 'n' ]
+  then
+    echo "Can not continue without install package."
+    exit 1
+fi
+
+if [[ $DOWNLOAD != "n" ]]
+  then
+    echo 'downloading latest'
+    rm -f teststudio-lite-*.tar;
+    curl -s https://api.github.com/repos/iqb-berlin/teststudio-lite-setup/releases/latest \
+    | grep "browser_download_url.*tar" \
+    | cut -d : -f 2,3 \
+    | tr -d \" \
+    | wget -qi -;
+fi
+
 ### Unpack application ###
 read  -p 'Install directory: ' -e -i "`pwd`/teststudio-lite" TARGET_DIR
 
@@ -57,31 +90,30 @@ sed -i "s/teststudio_lite_db_user/$POSTGRES_USER/" .env
 read  -p 'Database user password: ' -e -i $POSTGRES_PASSWORD POSTGRES_PASSWORD
 sed -i "s/iqb_tba_db_password_1/$POSTGRES_PASSWORD/" .env
 
-# read  -p 'Use TLS? (y/N): ' -e TLS
-# if [ $TLS = 'y' ]
-# then
-#   echo "The certificates need to be placed in config/certs and their name configured in config/cert_config.yml."
-#   sed -i 's/http:/https:/' .env
-#   sed -i 's/ws:/wss:/' .env
-# fi
+read  -p 'Use TLS? (y/N): ' -e TLS
+if [ $TLS = 'y' ]
+then
+  echo "The certificates need to be placed in config/certs and their name configured in config/cert_config.yml."
+  sed -i 's/http:/https:/' .env
+  sed -i 's/ws:/wss:/' .env
+fi
 
-### Populate Makefile ###
-# if [ $TLS = 'y' ]
-# then
-#   rm docker-compose.prod.nontls.yml
-#   sed -i 's/<run-command>/docker-compose -f docker-compose.yml -f docker-compose.prod.tls.yml up/' Makefile-template
-#   sed -i 's/<run-datached-command>/docker-compose -f docker-compose.yml -f docker-compose.prod.tls.yml up -d/' Makefile-template
-#   sed -i 's/<stop-command>/docker-compose -f docker-compose.yml -f docker-compose.prod.tls.yml stop/' Makefile-template
-#   sed -i 's/<down-command>/docker-compose -f docker-compose.yml -f docker-compose.prod.tls.yml down/' Makefile-template
-#   sed -i 's/<pull-command>/docker-compose -f docker-compose.yml -f docker-compose.prod.tls.yml pull/' Makefile-template
-# else
-  # rm docker-compose.prod.tls.yml
+## Populate Makefile ###
+if [ $TLS = 'y' ]
+then
+  sed -i 's/<run-command>/docker-compose -f docker-compose.yml -f docker-compose.prod.yml -f docker-compose.prod.tls.yml up/' Makefile-template
+  sed -i 's/<run-datached-command>/docker-compose -f docker-compose.yml -f docker-compose.prod.yml -f docker-compose.prod.tls.yml up -d/' Makefile-template
+  sed -i 's/<stop-command>/docker-compose -f docker-compose.yml -f docker-compose.prod.yml -f docker-compose.prod.tls.yml stop/' Makefile-template
+  sed -i 's/<down-command>/docker-compose -f docker-compose.yml -f docker-compose.prod.yml -f docker-compose.prod.tls.yml down/' Makefile-template
+  sed -i 's/<pull-command>/docker-compose -f docker-compose.yml -f docker-compose.prod.yml -f docker-compose.prod.tls.yml pull/' Makefile-template
+else
+  rm docker-compose.prod.tls.yml
   sed -i 's/<run-command>/docker-compose -f docker-compose.yml -f docker-compose.prod.yml up/' Makefile-template
   sed -i 's/<run-datached-command>/docker-compose -f docker-compose.yml -f docker-compose.prod.yml up -d/' Makefile-template
   sed -i 's/<stop-command>/docker-compose -f docker-compose.yml -f docker-compose.prod.yml stop/' Makefile-template
   sed -i 's/<down-command>/docker-compose -f docker-compose.yml -f docker-compose.prod.yml down/' Makefile-template
   sed -i 's/<pull-command>/docker-compose -f docker-compose.yml -f docker-compose.prod.yml pull/' Makefile-template
-# fi
+fi
 
 mv Makefile-template Makefile
 
