a23bb735294074c04a27e0e1e921d63c3ff8df9e
rhenck <richard.henck@iqb.hu-berlin.de>
2021-03-22 16:35:26 +0100

    Fix install script to correctly handle prompt value
    Was throwing error when inputting 'y'.

--- scripts/install.sh
+++ scripts/install.sh
@@ -34,7 +34,7 @@ else
 fi
 
 ### Download package ###
-DOWNLOAD='y'
+DOWNLOAD='y' # TODO  probably useless?
 if ls teststudio-lite-*.tar 1> /dev/null 2>&1
   then
     PACKAGE_FOUND=true
@@ -92,7 +92,7 @@ read  -p 'Database user password: ' -e -i $POSTGRES_PASSWORD POSTGRES_PASSWORD
 sed -i "s/iqb_tba_db_password_1/$POSTGRES_PASSWORD/" .env
 
 read  -p 'Use TLS? (y/N): ' -e TLS
-if [ $TLS = 'y' ]
+if [ $TLS != 'n' ]
 then
   echo "The certificates need to be placed in config/certs and their name configured in config/cert_config.yml."
   sed -i 's/http:/https:/' .env
@@ -100,7 +100,7 @@ then
 fi
 
 ## Populate Makefile ###
-if [ $TLS = 'y' ]
+if [ $TLS != 'n' ]
 then
   sed -i 's/<run-command>/docker-compose -f docker-compose.yml -f docker-compose.prod.yml -f docker-compose.prod.tls.yml up/' Makefile-template
   sed -i 's/<run-detached-command>/docker-compose -f docker-compose.yml -f docker-compose.prod.yml -f docker-compose.prod.tls.yml up -d/' Makefile-template
